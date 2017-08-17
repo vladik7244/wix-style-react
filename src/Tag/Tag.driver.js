bf083c0ff43d1e60dfda9a28154b158451cba47d
Tal Harel <talh@wix.com>
2017-08-17 18:54:18 +0300

    Test driver fixes

--- src/Tag/Tag.driver.js
+++ src/Tag/Tag.driver.js
@@ -2,6 +2,33 @@ import React from 'react';
 import ReactTestUtils from 'react-dom/test-utils';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import styles from './Tag.st.css';
+
+function hasCssState(elem, stylesheet, stateMap) {
+  if (!elem) {
+    return false;
+  }
+  const errors = [];
+  for (const k in stateMap) {
+    if (stateMap.hasOwnProperty(k)) {// eslint-disable-line no-prototype-builtins
+      const mapping = stylesheet.$stylesheet.cssStates({[k]: true});
+      if (stateMap[k]) {
+        for (const m in mapping) {
+          if (!elem.hasAttribute(m)) {
+            errors.push(`expected element to have state ":${k}" with mapping to "${m}" but got nothing.`);
+          }
+        }
+      } else {
+        for (const m in mapping) {
+          if (elem.hasAttribute(m)) {
+            errors.push(`expected element to not have state ":${k}" but found with mapping "${m}".`);
+          }
+        }
+      }
+    }
+  }
+  return !errors.length;
+}
 
 const tagDriverFactory = ({element, wrapper, component}) => {
 
@@ -12,15 +39,15 @@ const tagDriverFactory = ({element, wrapper, component}) => {
 
   return {
     exists: () => !!element,
-    isLarge: () => isClassExists(element, 'large'),
+    isLarge: () => hasCssState(element, styles, {large: true}),
     isStandardTheme: () => isClassExists(element, 'standard'),
     isWarningTheme: () => isClassExists(element, 'warning'),
     isErrorTheme: () => isClassExists(element, 'error'),
     isRemovable: () => isClassExists(removeButton, 'tagRemoveButton'),
     removeTag: () => ReactTestUtils.Simulate.click(removeButton),
     isThumbExists: () => isClassExists(thumb, 'thumb'),
-    isWrapped: () => isClassExists(element, 'tagWrap') && isClassExists(contentWithoutThumb, 'innerTagWrap'),
-    isDisabled: () => isClassExists(element, 'disabled'),
+    isWrapped: () => hasCssState(element, styles, {tagWrap: true}) && hasCssState(contentWithoutThumb, styles, {innerTagWrap: true}),
+    isDisabled: () => hasCssState(element, styles, {disabled: true}),
     getLabel: () => element.textContent,
     getTitle: () => element.title,
     setProps: props => {
