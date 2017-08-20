e3398cb0d3b9676baad0171ee091c34958b7a974
Tal Harel <talh@wix.com>
2017-08-20 17:34:48 +0300

    ToggleSwitch Stylable conversion (no test fixes)

---
+++ mock-st-css.js
@@ -0,0 +1,27 @@
+const namespace = 's' + Math.random().toString(36).slice(2);
+
+const proxy = new Proxy({}, {
+  get: (target, name) => {
+    if (name === '$stylesheet') {
+      return {
+        get: name => name,
+        cssStates: stateMapping => {
+          return stateMapping ? Object.keys(stateMapping).reduce((states, key) => {
+            if (stateMapping[key]) {
+              states['data-' + namespace.toLowerCase() + '-' + key.toLowerCase()] = true;
+            }
+            return states;
+          }, {}) : {};
+        }
+      };
+    }
+
+    if (name === 'default') {
+      return proxy;
+    }
+
+    return name;
+  }
+});
+
+module.exports = proxy;
--- src/ToggleSwitch/ToggleSwitch.driver.js
+++ src/ToggleSwitch/ToggleSwitch.driver.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import ToggleSwitch from './ToggleSwitch';
+import ToggleSwitch from './index';
 
 export default class ToggleSwitchDriver {
   constructor() {
