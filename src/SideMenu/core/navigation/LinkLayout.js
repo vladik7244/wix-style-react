75ace72450f3a76a515a138c6dbb31317bcb3175
Eyal Eizenberg <eyale@wix.com>
2017-08-29 10:31:53 +0300

    allow sending additional classnames

--- src/SideMenu/core/navigation/LinkLayout.js
+++ src/SideMenu/core/navigation/LinkLayout.js
@@ -3,18 +3,24 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import styles from './styles.scss';
 
-const LinkLayout = ({children, isDiminishedHover, isActive, ...rest}) =>
-  <span
-    className={classnames({
-      [styles.linkLayout]: true,
-      [styles.linkActive]: isActive,
-      [styles.linkDiminishedHover]: isDiminishedHover
-    })}
-    data-hook="menu-navigation-link-wrapper"
-    {...rest}
-    >
-    {children}
-  </span>;
+const LinkLayout = ({children, isDiminishedHover, isActive, ...rest}) => {
+  const {className, ...additionals} = rest;
+  return (
+      <span
+        className={classnames(
+          className,
+        {
+          [styles.linkLayout]: true,
+          [styles.linkActive]: isActive,
+          [styles.linkDiminishedHover]: isDiminishedHover,
+        })}
+        data-hook="menu-navigation-link-wrapper"
+        {...additionals}
+        >
+        {children}
+      </span>;
+  );
+}
 
 LinkLayout.propTypes = {
   children: PropTypes.node,
