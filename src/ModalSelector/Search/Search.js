a21bea8db5d2684d796a7535be1b7a8772df6705
Nir Hartmann <nirh@wix.com>
2017-09-04 15:39:40 +0300

    fix search style

--- src/ModalSelector/Search/Search.js
+++ src/ModalSelector/Search/Search.js
@@ -1,34 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Search.scss';
 import WixComponent from '../../BaseComponents/WixComponent';
-import Input from '../../Input/Input';
+import Input from '../../Input';
+import styles from './Search.scss';
+import Finder from '../../Icons/dist/components/Finder';
 
 class Search extends WixComponent {
   static propTypes = {
-    // isOpen: PropTypes.bool.isRequired,
-    // onRequestClose: PropTypes.func,
-    // onOk: PropTypes.func,
-    // onCancel: PropTypes.func
-  }
+  };
 
   static defaultProps = {
-  }
+  };
 
   render() {
-    // const {} = this.props;
 
     return (
       <Input
-        className={styles.input}
         id="search-input"
-        maxLength={524288}
         placeholder="Search..."
-        size="normal"
-        textOverflow="clip"
-        theme="normal"
-        width="initial"
-        magnifyingGlass
+        prefix = {
+          <div className={styles.search}>
+            <Finder/>
+          </div>
+        }
         roundInput
         />
     );
