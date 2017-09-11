0280b8a86a243dd654aed104f6759dbe8558f59c
Tal Harel <talh@wix.com>
2017-09-11 16:19:25 -0400

    Lint fixes

--- src/stylable-has-css-state.js
+++ src/stylable-has-css-state.js
@@ -4,19 +4,20 @@ function hasCssState(elem, stylesheet, stateMap) {
   }
   const errors = [];
   for (const k in stateMap) {
-    if (stateMap.hasOwnProperty(k)) {// eslint-disable-line no-prototype-builtins
-      const mapping = stylesheet.$stylesheet.cssStates({[k]: true});
-      if (stateMap[k]) {
-        for (const m in mapping) {
-          if (!elem.hasAttribute(m)) {
-            errors.push(`expected element to have state ":${k}" with mapping to "${m}" but got nothing.`);
-          }
+    if (!stateMap.hasOwnProperty(k)) { // eslint-disable-line no-prototype-builtins
+      continue;
+    }
+    const mapping = stylesheet.$stylesheet.cssStates({[k]: true});
+    if (stateMap[k]) {
+      for (const m in mapping) {
+        if (!elem.hasAttribute(m)) {
+          errors.push(`expected element to have state ":${k}" with mapping to "${m}" but got nothing.`);
         }
-      } else {
-        for (const m in mapping) {
-          if (elem.hasAttribute(m)) {
-            errors.push(`expected element to not have state ":${k}" but found with mapping "${m}".`);
-          }
+      }
+    } else {
+      for (const m in mapping) {
+        if (elem.hasAttribute(m)) {
+          errors.push(`expected element to not have state ":${k}" but found with mapping "${m}".`);
         }
       }
     }
