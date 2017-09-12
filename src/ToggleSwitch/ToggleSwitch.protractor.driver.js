0a7b12ff4323cf3a8ab97c0677a608837452e00b
Tal Harel <talh@wix.com>
2017-09-12 18:52:28 -0400

    ToggleSwitch protractor driver fix

--- src/ToggleSwitch/ToggleSwitch.protractor.driver.js
+++ src/ToggleSwitch/ToggleSwitch.protractor.driver.js
@@ -1,13 +1,34 @@
-import styles from './ToggleSwitch.st.css';
-import {hasCssState} from '../stylable-has-css-state';
+const STYLABLE_DIVIDER_LENGTH = 2;
+
+const getNamespace = component => {
+  return component.getAttribute('class').then(classes => {
+    const rootMatch = classes.match(/(ToggleSwitch.*)root/);
+    if (!rootMatch) {
+      throw new Error('Invalid root class: ' + classes);
+    }
+    const rootGroupMatch = rootMatch[1];
+    return rootGroupMatch.slice(0, rootGroupMatch.length - STYLABLE_DIVIDER_LENGTH);
+  });
+};
+
+const hasCssState = (component, stateName, expected = true) =>
+  getNamespace(component)
+      .then(namespace => component.getAttribute(`data-${namespace.toLowerCase()}-${stateName.toLowerCase()}`))
+      .then(value => {
+        const valueExpr = value === 'true';
+        if (expected !== valueExpr) {
+          throw new Error(`style-state ${stateName} expected: ${expected} but got ${valueExpr}`);
+        }
+        return true;
+      });
 
 const toggleSwitchDriverFactory = component => ({
   click: () => component.click(),
   element: () => component,
   checked: () => component.$('input').isSelected(),
-  isXSmall: () => hasCssState(element, styles, {toggleSwitchXSmall: true}),
-  isSmall: () => hasCssState(element, styles, {toggleSwitchSmall: true}),
-  isLarge: () => !hasCssState(element, styles, {toggleSwitchXSmall: true}) && !hasCssState(element, styles, {toggleSwitchSmall: true}),
+  isXSmall: () => hasCssState(component, 'toggleSwitchXSmall'),
+  isSmall: () => hasCssState(component, 'toggleSwitchSmall'),
+  isLarge: () => hasCssState(component, 'toggleSwitchSmall', false).then(() => hasCssState(component, 'toggleSwitchXSmall', false))
 });
 
 export default toggleSwitchDriverFactory;
