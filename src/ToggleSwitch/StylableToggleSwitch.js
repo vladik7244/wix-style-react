5120a1f0acf32232c4dec4bc8c0649959954fe28
Tal Harel <talh@wix.com>
2017-09-12 15:32:03 -0400

    ToggleSwitch wix-react-tools integration

--- src/ToggleSwitch/StylableToggleSwitch.js
+++ src/ToggleSwitch/StylableToggleSwitch.js
@@ -3,10 +3,26 @@ import PropTypes from 'prop-types';
 import uniqueId from 'lodash/uniqueId';
 import WixComponent from '../BaseComponents/WixComponent';
 
-import {SBComponent as sbcomponent} from 'stylable-react-component';
+import {stylable} from 'wix-react-tools';
 import styles from './ToggleSwitch.st.css';
 
-class ToggleSwitch extends WixComponent {
+@stylable(styles)
+export default class ToggleSwitch extends WixComponent {
+
+  static displayName = 'ToggleSwitch';
+  static propTypes = {
+    checked: PropTypes.bool.isRequired,
+    onChange: PropTypes.func.isRequired,
+    disabled: PropTypes.bool,
+    size: PropTypes.oneOf(['x-small', 'small', 'large'])
+  };
+
+  static defaultProps = {
+    checked: false,
+    onChange: () => { },
+    disabled: false,
+    size: 'large'
+  };
 
   constructor(params) {
     super(params);
@@ -18,14 +34,14 @@ class ToggleSwitch extends WixComponent {
     const id = this.id;
 
     return (
-      <div cssStates={{toggleSwitchSmall: size === 'small', toggleSwitchXSmall: size === 'x-small', disabled}}>
+      <div style-state={{toggleSwitchSmall: size === 'small', toggleSwitchXSmall: size === 'x-small', disabled}}>
         <input type="checkbox" id={id} className="inputCheckbox" checked={checked} onChange={onChange}/>
-        <label htmlFor={id} cssStates={{disabled}} className="outerLabel">
-          <label htmlFor={id} cssStates={{disabled}} className="innerLabel">
-            <svg cssStates={{disabled}} className="toggleActive" viewBox="0 0 41 32">
+        <label htmlFor={id} style-state={{disabled}} className="outerLabel">
+          <label htmlFor={id} style-state={{disabled}} className="innerLabel">
+            <svg style-state={{disabled}} className="toggleActive" viewBox="0 0 41 32">
               <path d="M0.169 17.815c0.169 1.098 0.76 2.111 1.689 2.871l14.269 10.385c1.942 1.435 4.644 1.013 6.079-0.844l18.069-23.303c1.435-1.858 1.098-4.559-0.844-5.995s-4.644-1.098-6.164 0.844l-15.367 19.842-10.723-7.852c-1.942-1.435-4.644-1.013-6.164 0.844-0.76 0.929-1.013 2.111-0.844 3.208z"/>
             </svg>
-            <svg cssStates={{disabled}} className="toggleInactive" viewBox="0 0 143 32">
+            <svg style-state={{disabled}} className="toggleInactive" viewBox="0 0 143 32">
               <path d="M0 0h142.545v32h-142.545v-32z"/>
             </svg>
           </label>
@@ -34,20 +50,3 @@ class ToggleSwitch extends WixComponent {
     );
   }
 }
-
-ToggleSwitch.displayName = 'ToggleSwitch';
-ToggleSwitch.propTypes = {
-  checked: PropTypes.bool.isRequired,
-  onChange: PropTypes.func.isRequired,
-  disabled: PropTypes.bool,
-  size: PropTypes.oneOf(['x-small', 'small', 'large'])
-};
-
-ToggleSwitch.defaultProps = {
-  checked: false,
-  onChange: () => { },
-  disabled: false,
-  size: 'large'
-};
-
-export default sbcomponent(ToggleSwitch, styles);
